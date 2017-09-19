4bf5594004896e0ad966cf3d9ac962ee268aecd0
dwicao <lutfiandwica@gmail.com>
2017-09-20 06:40:04 +0700

    add RN props for View

--- app/uikit/Primitive.js
+++ app/uikit/Primitive.js
@@ -15,16 +15,79 @@ if (!IS_WEB) {
 
 class ViewPrimitive extends Component {
   render() {
+    const {
+      accessibilityLabel,
+      accessible,
+      hitSlop,
+      nativeID,
+      onAccessibilityTap,
+      onLayout,
+      onMagicTap,
+      onMoveShouldSetResponder,
+      onMoveShouldSetResponderCapture,
+      onResponderGrant,
+      onResponderMove,
+      onResponderReject,
+      onResponderRelease,
+      onResponderTerminate,
+      onResponderTerminationRequest,
+      onStartShouldSetResponder,
+      onStartShouldSetResponderCapture,
+      pointerEvents,
+      removeClippedSubviews,
+      testID,
+      accessibilityComponentType,
+      accessibilityLiveRegion,
+      collapsable,
+      importantForAccessibility,
+      needsOffscreenAlphaCompositing,
+      renderToHardwareTextureAndroid,
+      accessibilityTraits,
+      accessibilityViewIsModal,
+      shouldRasterizeIOS,
+      ...otherProps,
+    } = this.props;
+
     if (IS_WEB) {
       return (
-        <div {...this.props}>
+        <div {...otherProps}>
           {this.props.children}
         </div>
       );
     } else {
       return (
-        <View {...this.props}>
-          {this.props.children}
+        <View
+          accessibilityLabel={accessibilityLabel}
+          accessible={accessible}
+          hitSlop={hitSlop}
+          nativeID={nativeID}
+          onAccessibilityTap={onAccessibilityTap}
+          onLayout={onLayout}
+          onMagicTap={onMagicTap}
+          onMoveShouldSetResponder={onMoveShouldSetResponder}
+          onMoveShouldSetResponderCapture={onMoveShouldSetResponderCapture}
+          onResponderGrant={onResponderGrant}
+          onResponderMove={onResponderMove}
+          onResponderReject={onResponderReject}
+          onResponderRelease={onResponderRelease}
+          onResponderTerminate={onResponderTerminate}
+          onResponderTerminationRequest={onResponderTerminationRequest}
+          onStartShouldSetResponder={onStartShouldSetResponder}
+          onStartShouldSetResponderCapture={onStartShouldSetResponderCapture}
+          pointerEvents={pointerEvents}
+          removeClippedSubviews={removeClippedSubviews}
+          testID={testID}
+          accessibilityComponentType={accessibilityComponentType}
+          accessibilityLiveRegion={accessibilityLiveRegion}
+          collapsable={collapsable}
+          importantForAccessibility={importantForAccessibility}
+          needsOffscreenAlphaCompositing={needsOffscreenAlphaCompositing}
+          renderToHardwareTextureAndroid={renderToHardwareTextureAndroid}
+          accessibilityTraits={accessibilityTraits}
+          accessibilityViewIsModal={accessibilityViewIsModal}
+          shouldRasterizeIOS={shouldRasterizeIOS}
+          {...otherProps}>
+            {this.props.children}
         </View>
       );
     }
